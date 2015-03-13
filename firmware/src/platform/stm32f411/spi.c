77eed017e95498248ff9ead1b5797d0f7fb601e2
Sean Wan <swan@google.com>
2015-03-13 10:48:52 -0700

    spi: add definition of SPI2.

--- firmware/src/platform/stm32f411/spi.c
+++ firmware/src/platform/stm32f411/spi.c
@@ -298,8 +298,25 @@ static struct StmSpiDev mStmSpiDevs[] = {
             .irq = SPI1_IRQn,
         },
     },
+    [1] = {
+        .cfg = {
+            .regs = (struct StmSpi *)SPI2_BASE,
+
+            .clokcBus = PERIPH_BUS_APB1,
+            .clockUnit = PERIPH_APB1_SPI2,
+
+            .gpioMiso = GPIO_PB(14),
+            .gpioMosi = GPIO_PB(15),
+            .gpioSclk = GPIO_PB(13),
+            .gpioNss = GPIO_PB(12),
+            .gpioFunc = GPIO_A2_AFR_SPI123,
+
+            .irq = SPI2_IRQn,
+        },
+    },
 };
 DECLARE_IRQ_HANDLER(1)
+DECLARE_IRQ_HANDLER(2)
 
 static inline void stmSpiGpioInit(struct Gpio *gpio,
         GpioNum number, uint8_t func)
