37c192668f301d0e029ad1b4ba553bfb23321f52
Heather Lee Wilson <hwilson@google.com>
2015-03-16 17:52:34 -0700

    Fix for locks
    Via dmitrygr - Fix to force GCC to use same register during
    exclusive load/store locking mechanism.
    
    Change-Id: I8c1109e21760e8cb6de50da903eb82485d9d35af

--- firmware/src/cpu/cortexm4f/atomicXchg.c
+++ firmware/src/cpu/cortexm4f/atomicXchg.c
@@ -1,7 +1,7 @@
 #include <cpu/inc/atomicXchg.h>
 
 
-uint8_t atomicXchgByte(volatile uint8_t *byte, uint8_t newVal)
+uint32_t atomicXchgByte(volatile uint8_t *byte, uint32_t newVal)
 {
     uint32_t prevVal, storeFailed;
 
@@ -9,8 +9,8 @@ uint8_t atomicXchgByte(volatile uint8_t *byte, uint8_t newVal)
         asm volatile(
             "ldrexb %0,     [%3] \n"
             "strexb %1, %2, [%3] \n"
-            :"=r"(prevVal), "=r"(storeFailed), "=r"(newVal)
-            :"2"(newVal), "r"(byte)
+            :"=r"(prevVal), "=r"(storeFailed), "=r"(newVal), "=r"(byte)
+            :"2"(newVal), "3"(byte)
             :"memory"
         );
     } while (storeFailed);
@@ -18,7 +18,7 @@ uint8_t atomicXchgByte(volatile uint8_t *byte, uint8_t newVal)
     return prevVal;
 }
 
-uint32_t atomicXchg32bits(volatile uint32_t *byte, uint32_t newVal)
+uint32_t atomicXchg32bits(volatile uint32_t *word, uint32_t newVal)
 {
     uint32_t prevVal, storeFailed;
 
@@ -26,8 +26,8 @@ uint32_t atomicXchg32bits(volatile uint32_t *byte, uint32_t newVal)
         asm volatile(
             "ldrex %0,     [%3] \n"
             "strex %1, %2, [%3] \n"
-            :"=r"(prevVal), "=r"(storeFailed), "=r"(newVal)
-            :"2"(newVal), "r"(byte)
+            :"=r"(prevVal), "=r"(storeFailed), "=r"(newVal), "=r"(word)
+            :"2"(newVal), "3"(word)
             :"memory"
         );
     } while (storeFailed);
