f71783794da48475c42d2348b78f3ef95039963c
Dmitry Grinberg <dmitrygr@google.com>
2015-04-08 15:57:54 -0700

    cpu_cm4f: implement atatic alloc for atomic bitset
    Change-Id: I3e69d038f39d7464be692e9e4e32d9f59ab9bd1b

--- firmware/inc/cpu/cortexm4f/atomicBitset.h
+++ firmware/inc/cpu/cortexm4f/atomicBitset.h
@@ -10,6 +10,7 @@ struct AtomicBitset {
 };
 
 #define ATOMIC_BITSET_SZ(numbits)	(sizeof(struct AtomicBitset) + ((numbits) + 31) / 8)
+#define ATOMIC_BITSET_DECL(nam, numbits, extra_keyword)    extra_keyword uint8_t _##nam##_store [ATOMIC_BITSET_SZ(numbits)] __attribute__((aligned(4))); extra_keyword struct AtomicBitset *nam = (struct AtomicBitset*)_##nam##_store
 
 
 void atomicBitsetInit(struct AtomicBitset *set, uint32_t numBits);
