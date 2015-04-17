a30d6ea5f4f3d146ad712a16fae6b951eb518772
Dmitry Grinberg <dmitrygr@google.com>
2015-04-17 13:52:14 -0700

    cpu_cm4f: enable fpu
    Change-Id: Ifb0591e27446133b42b99339a1d21a3d7b9d046f

--- firmware/src/cpu/cortexm4f/cpu.c
+++ firmware/src/cpu/cortexm4f/cpu.c
@@ -6,6 +6,9 @@ void cpuInit(void)
 {
     /* set pendsv to be lowest priority possible */
     SCB->SHP[3] = (SCB->SHP[3] & 0xFF00FFFF) | 0x00010000;
+
+    /* FPU on */
+    SCB->CPACR |= 0x00F00000;
 }
 
 uint64_t cpuIntsOff(void)
