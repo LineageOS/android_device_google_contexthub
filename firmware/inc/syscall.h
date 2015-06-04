1fddca98554f2ca3b084605fa82062823557ecce
Dmitry Grinberg <dmitrygr@google.com>
2015-06-03 18:57:34 -0700

    syscall: part 1 of 2 of exporting event queue and logging to external callers
    Change-Id: Ifc7c81c8bedeb9c2b684d12dfe9e97d5e17a6aa3

--- firmware/inc/syscall.h
+++ firmware/inc/syscall.h
@@ -28,11 +28,11 @@ extern "C" {
 
 
 //level 0 indices:
-#define SYSCALL_DOMAIN_SEOS       0
+#define SYSCALL_DOMAIN_OS         0
 #define SYSCALL_DOMAIN_DRIVERS    1
 
 
-typedef uint32_t (*SyscallFunc)(va_list args); /* you better know what args you need */
+typedef void (*SyscallFunc)(uint32_t *retValP, va_list args); /* you better know what args you need */
 
 struct SyscallTable {
     uint32_t numEntries;
