396f58ffc174cc626b1bd9df2290504c4525b936
Greg Hackmann <ghackmann@google.com>
2015-06-25 13:08:24 -0700

    stm32f4xx: spi: make some configuration data variant-specific
    Several parts of the SPI configuration are tied to how the GPIOs are
    configured, which is specific to the board layout.  Split this data off
    into a separate struct StmSpiPinCfg and require variant support code to
    supply this information.
    
    Also supply the appropriate support code for the pike board variant.
    
    Change-Id: Ia5ff9323692b649a8d3e9b1af2fa231c686fd600
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

---
+++ firmware/inc/platform/stm32f4xx/spi.h
@@ -0,0 +1,21 @@
+#ifndef __PLAT_SPI_H
+#define __PLAT_SPI_H
+
+#include <gpio.h>
+#include <plat/inc/cmsis.h>
+#include <plat/inc/gpio.h>
+
+struct StmSpiPinCfg {
+    GpioNum gpioMiso;
+    GpioNum gpioMosi;
+    GpioNum gpioSclk;
+    GpioNum gpioNss;
+
+    enum GpioAltFunc gpioFunc;
+
+    IRQn_Type irqNss;
+};
+
+extern const struct StmSpiPinCfg *boardStmSpiPinCfg(uint8_t busId);
+
+#endif /* __PLAT_SPI_H */
