967ae4c75aff3875de61311d9808719404d9dbb1
Ben Fennema <fennema@google.com>
2015-07-08 11:23:41 -0700

    variant: add angler and bullhead
    Change-Id: Ib70e6d4e7951b02b3e51340316cc434717cbcf00
    Signed-off-by: Ben Fennema <fennema@google.com>

---
+++ firmware/inc/variant/bullhead/variant.h
@@ -0,0 +1,23 @@
+#ifndef _VARIANT_BULLHEAD_H_
+#define _VARIANT_BULLHEAD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//we have LSE in bullhead
+#define HAVE_LSE    true
+
+//spi bus for comms
+#define PLATFORM_HOST_INTF_SPI_BUS  0
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
