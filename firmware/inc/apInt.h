67edb1d56c071082e4ebf31fc751a969cff43a5a
Ben Fennema <fennema@google.com>
2015-07-09 15:38:52 -0700

    apInt: add support for sending interrupts to the ap
    A non-masked bit being set will cause a wakeup interrupt to be generated.
    A masked bit being set will cause a non-wakeup interrupt to be generated
    if the board supports it. Otherwise no interrupt will be generated.
    
    Change-Id: I58fed3b7b34e840c47af6e3e799c56eb14df24dd
    Signed-off-by: Ben Fennema <fennema@google.com>

---
+++ firmware/inc/apInt.h
@@ -0,0 +1,17 @@
+#ifndef _APINT_H_
+#define _APINT_H_
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void apIntSet(bool wakeup);
+void apIntClear(bool wakeup);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- firmware/inc/variant/bullhead/variant.h
+++ firmware/inc/variant/bullhead/variant.h
@@ -6,18 +6,16 @@ extern "C" {
 #endif
 
 //we have LSE in bullhead
-#define HAVE_LSE    true
+#define HAVE_LSE                    true
 
 //spi bus for comms
 #define PLATFORM_HOST_INTF_SPI_BUS  0
 
-
-
-
+#define AP_INT_WAKEUP               GPIO_PA(3)
+#undef AP_INT_NONWAKEUP
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif
-
