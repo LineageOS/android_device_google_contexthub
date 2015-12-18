1d13190d84dfcb2ea874a0d2f792b85022b1ec6a
Dmitry Grinberg <dmitrygr@google.com>
2015-12-18 12:50:21 -0800

    Inline simple functions for speed
    Because function call overhead should not be an issue
    
    Change-Id: I863af45a9294aa0708094f435f8bc97057aeda7a

--- firmware/inc/fusion/vec.h
+++ firmware/inc/fusion/vec.h
@@ -1,6 +1,7 @@
 #ifndef VEC_H_
-
 #define VEC_H_
+
+#include <nanohub_math.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -14,19 +15,67 @@ struct Vec4 {
     float x, y, z, w;
 };
 
-void initVec3(struct Vec3 *v, float x, float y, float z);
-void vec3Add(struct Vec3 *v, const struct Vec3 *w);
-void vec3Sub(struct Vec3 *v, const struct Vec3 *w);
-void vec3ScalarMul(struct Vec3 *v, float c);
-float vec3Dot(const struct Vec3 *v, const struct Vec3 *w);
-float vec3NormSquared(const struct Vec3 *v);
-float vec3Norm(const struct Vec3 *v);
-void vec3Normalize(struct Vec3 *v);
-void vec3Cross(struct Vec3 *u, const struct Vec3 *v, const struct Vec3 *w);
+
+static inline void initVec3(struct Vec3 *v, float x, float y, float z) {
+    v->x = x;
+    v->y = y;
+    v->z = z;
+}
+
+static inline void vec3Add(struct Vec3 *v, const struct Vec3 *w) {
+    v->x += w->x;
+    v->y += w->y;
+    v->z += w->z;
+}
+
+static inline void vec3Sub(struct Vec3 *v, const struct Vec3 *w) {
+    v->x -= w->x;
+    v->y -= w->y;
+    v->z -= w->z;
+}
+
+static inline void vec3ScalarMul(struct Vec3 *v, float c) {
+    v->x *= c;
+    v->y *= c;
+    v->z *= c;
+}
+
+static inline float vec3Dot(const struct Vec3 *v, const struct Vec3 *w) {
+    return v->x * w->x + v->y * w->y + v->z * w->z;
+}
+
+static inline float vec3NormSquared(const struct Vec3 *v) {
+    return vec3Dot(v, v);
+}
+
+static inline float vec3Norm(const struct Vec3 *v) {
+    return sqrtf(vec3NormSquared(v));
+}
+
+static inline void vec3Normalize(struct Vec3 *v) {
+    float invNorm = 1.0f / vec3Norm(v);
+    v->x *= invNorm;
+    v->y *= invNorm;
+    v->z *= invNorm;
+}
+
+static inline void vec3Cross(struct Vec3 *u, const struct Vec3 *v, const struct Vec3 *w) {
+    u->x = v->y * w->z - v->z * w->y;
+    u->y = v->z * w->x - v->x * w->z;
+    u->z = v->x * w->y - v->y * w->x;
+}
+
+static inline void initVec4(struct Vec4 *v, float x, float y, float z, float w) {
+    v->x = x;
+    v->y = y;
+    v->z = z;
+    v->w = w;
+}
+
+
 void findOrthogonalVector( float inX, float inY, float inZ,
         float *outX, float *outY, float *outZ);
 
-void initVec4(struct Vec4 *v, float x, float y, float z, float w);
 
 #ifdef __cplusplus
 }
