24ee3e3768ec9fcf0066bf0a2ea205f8ccc02d3f
Andrew Rossignol <aarossig@google.com>
2016-03-01 12:41:52 -0800

    nanotool: Fixed the file size logic
    Change-Id: I2ba09d47e371225115cacb00f9c4453f0b1083fe

--- util/nanotool/contexthub.cpp
+++ util/nanotool/contexthub.cpp
@@ -113,14 +113,15 @@ bool ContextHub::Flash(const std::string& filename) {
         return false;
     }
 
-    int file_size = fseek(firmware_file, 0, SEEK_END);
+    fseek(firmware_file, 0, SEEK_END);
+    long file_size = ftell(firmware_file);
     fseek(firmware_file, 0, SEEK_SET);
 
     auto firmware_data = std::vector<uint8_t>(file_size);
     size_t bytes_read = fread(firmware_data.data(), sizeof(uint8_t),
         file_size, firmware_file);
     if (bytes_read != static_cast<size_t>(file_size)) {
-        LOGE("Read of firmware file returned %zu, expected %d",
+        LOGE("Read of firmware file returned %zu, expected %ld",
             bytes_read, file_size);
         return false;
     }
