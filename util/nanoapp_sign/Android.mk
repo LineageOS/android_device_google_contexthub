cc65ff94cc6905e94b897dc7b64929c720de650e
Alexey Polyudov <apolyudov@google.com>
2016-04-28 15:54:39 -0700

    contexthub: util: sign: make it aware of AOSP header
    Bug: 28265099
    Change-Id: I79b948217885019e376fac54937330f7963ee97e
    Signed-off-by: Alexey Polyudov <apolyudov@google.com>

--- util/nanoapp_sign/Android.mk
+++ util/nanoapp_sign/Android.mk
@@ -14,15 +14,33 @@
 # limitations under the License.
 #
 
-APP = nanoapp_sign
-SRC = nanoapp_sign.c ../../lib/nanohub/rsa.c ../../lib/nanohub/sha2.c
-CC ?= gcc
-CC_FLAGS = -Wall -Werror -Wextra
+LOCAL_PATH := $(call my-dir)
 
-$(APP): $(SRC) Makefile
-	$(CC) $(CC_FLAGS) -o $(APP) -O2 $(SRC) \
-	        -I../../lib/include \
-	        -DRSA_SUPPORT_PRIV_OP_BIGRAM -DHOST_BUILD -DBOOTLOADER= -DBOOTLOADER_RO=
+include $(CLEAR_VARS)
 
-clean:
-	rm -f $(APP)
+LOCAL_SRC_FILES := \
+    ../../lib/nanohub/rsa.c \
+    ../../lib/nanohub/sha2.c \
+    ../../lib/nanohub/nanoapp.c \
+    nanoapp_sign.c \
+
+
+LOCAL_CFLAGS := \
+    -Wall \
+    -Werror \
+    -Wextra \
+    -DRSA_SUPPORT_PRIV_OP_BIGRAM \
+    -DHOST_BUILD \
+    -DBOOTLOADER= \
+    -DBOOTLOADER_RO= \
+
+
+LOCAL_C_INCLUDES := \
+    device/google/contexthub/lib/include \
+
+
+LOCAL_MODULE := nanoapp_sign
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_HOST_EXECUTABLE)
