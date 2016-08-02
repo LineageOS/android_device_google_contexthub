ea1a983adc41f717aca2d648648f316cb6f11a07
Alexey Polyudov <apolyudov@google.com>
2016-08-01 22:17:32 -0700

    libm: rearrange files
    Change-Id: I93c0a52156d32c5cba5835f087b366eb7f052a36

--- firmware/lib/lib.mk
+++ firmware/lib/lib.mk
@@ -13,6 +13,7 @@ CFLAGS += -D_IEEE_LIBM
 SRCS += $(LIBM_PATH)/ef_atan2.c
 SRCS += $(LIBM_PATH)/ef_asin.c
 SRCS += $(LIBM_PATH)/ef_fmod.c
+SRCS += $(LIBM_PATH)/ef_pow.c
 SRCS += $(LIBM_PATH)/ef_rem_pio2.c
 SRCS += $(LIBM_PATH)/ef_sqrt.c
 SRCS += $(LIBM_PATH)/kf_cos.c
@@ -28,6 +29,7 @@ SRCS += $(LIBM_PATH)/sf_sin.c
 SRCS += $(LIBM_PATH)/wf_atan2.c
 SRCS += $(LIBM_PATH)/wf_asin.c
 SRCS += $(LIBM_PATH)/wf_fmod.c
+SRCS += $(LIBM_PATH)/wf_pow.c
 
 # Libc #########################################################################
 
